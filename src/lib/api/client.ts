807a794ada4b135079fb3874253b3162d6512678
Abhishek <abhishek.singh@cosmostaker.com>
2025-04-07 19:01:17 +0530

    Fixed vercel issue

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -7,10 +7,8 @@ class ApiClient {
   private client: AxiosInstance;
 
   constructor() {
-    console.log(API_BASE_URL,'API_BASE_URL');
     this.client = axios.create({
       baseURL: API_BASE_URL,
-      // baseURL: "https://b8fc-116-72-200-116.ngrok-free.app/v1",
       headers: {
         'Content-Type': 'application/json',
       },
